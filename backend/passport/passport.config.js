90d1f0324bbcd2459f49d266a71e4cc9161dce36
darmal <d.gulabzoi@gmail.com>
2024-07-10 17:35:41 -0300

    fix passport issues

--- backend/passport/passport.config.js
+++ backend/passport/passport.config.js
@@ -1,36 +1,37 @@
-import bcrypt from "bcryptjs";
-import { GraphQLLocalStrategy } from "graphql-passport";
 import passport from "passport";
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { GraphQLLocalStrategy } from "graphql-passport";
 
 export const configurePassport = async () => {
   passport.serializeUser((user, done) => {
-    console.log("serialized user");
+    console.log("Serializing user");
     done(null, user.id);
   });
   passport.deserializeUser(async (id, done) => {
-    console.log("deserialized user");
+    console.log("Deserializing user");
     try {
       const user = await User.findById(id);
       done(null, user);
-    } catch (error) {
-      done(error);
+    } catch (err) {
+      done(err);
     }
   });
 
   passport.use(
-    new GraphQLLocalStrategy(async (username, passport, done) => {
+    new GraphQLLocalStrategy(async (username, password, done) => {
       try {
-        const user = await User.findById({ username });
+        const user = await User.findOne({ username });
         if (!user) {
-          throw new Error("invalid username or password");
+          throw new Error("Invalid username or password");
         }
         const validPassword = await bcrypt.compare(password, user.password);
         if (!validPassword) {
-          throw new Error("invalid username or password");
+          throw new Error("Invalid username or password");
         }
         return done(null, user);
-      } catch (error) {
-        return done(error);
+      } catch (err) {
+        return done(err);
       }
     })
   );
